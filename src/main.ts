a5361e56f963c4edbe25acdab5af54c5adaa53dd
Dmytro <kotykhin_d@ukr.net>
2023-01-24 23:08:29 +0100

    cors config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3001;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT, () =>
     console.log(`>>> Server started on port ${PORT}`),
